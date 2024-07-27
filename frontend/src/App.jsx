f33ff4f6d90be753312888fb8bf8687ac3d8d966
PankajLamgria <pankajlamgria@gmail.com>
2024-07-27 23:55:31 +0530

    Signin Login Form Added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import {BrowserRouter as Router,Route,Routes } from 'react-router-dom';
 import UserSignin from "./components/Signin/UserSignin.jsx";
+import UserLogin from "./components/Signin/UserLogin.jsx";
 const App = () => {
   return(
     <Router>
       <Routes>
         <Route path="/UserSignin" element={<UserSignin/>}/>
+        <Route path="/UserLogin" element={<UserLogin/>}/>
+        
       </Routes>
     </Router>
   )
