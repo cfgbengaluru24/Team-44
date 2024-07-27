5f4284aa470d619d9024701a7546d05fbded6a0f
PankajLamgria <pankajlamgria@gmail.com>
2024-07-27 21:07:27 +0530

    App setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
-
+import {BrowserRouter as Router,Route,Routes } from 'react-router-dom';
+import UserSignin from "./components/Signin/UserSignin.jsx";
 const App = () => {
-  return <div></div>;
+  return(
+    <Router>
+      <Routes>
+        <Route path="/UserSignin" element={<UserSignin/>}/>
+      </Routes>
+    </Router>
+  )
 };
 
 export default App;
---
+++ frontend/src/components/Signin/UserSignin.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const UserSignin = () => {
+  return (
+    <div>
+       
+      
+    </div>
+  )
+}
+
+export default UserSignin
