b23f26539053f8f8c3a7832ef835ea4a9cb681e8
PankajLamgria <pankajlamgria@gmail.com>
2024-07-27 18:20:00 +0530

    Set up Backend

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express=require('express');
+{require("dotenv").config();}
+const app=express();
+
+const cors=require("cors");
+app.use(cors());
+app.use(express.json());
+const port =process.env.PORT || 4000;
+
+// app.use('/api/auth',require("./routes/auth.js"));
+
+app.listen(port,()=>{
+    console.log(`server running on port http://localhost:${port}`);
+})
