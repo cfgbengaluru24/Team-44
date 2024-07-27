d7a21a63dd43704313fc591108dfc28f703ea7cb
swapnil6504 <swapnilchhibber6504@gmail.com>
2024-07-27 22:46:23 +0530

    add navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import {BrowserRouter as Router,Route,Routes } from 'react-router-dom';
 import UserSignin from "./components/Signin/UserSignin.jsx";
+import Recruitment from "./components/RecruitmentProcess/Recruitment.jsx";
 const App = () => {
   return(
     <Router>
       <Routes>
         <Route path="/UserSignin" element={<UserSignin/>}/>
+        <Route path="/StudentHome" element={<Recruitment/>}/>
       </Routes>
     </Router>
   )
