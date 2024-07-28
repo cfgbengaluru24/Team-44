2d5a768f906417c71e04470130d62f72ea19a15a
PankajLamgria <pankajlamgria@gmail.com>
2024-07-28 05:34:14 +0530

    Updated Navbar

--- frontend/src/context/Projectstate.jsx
+++ frontend/src/context/Projectstate.jsx
@@ -3,9 +3,10 @@ import projectcontext from './Projectcontext.jsx'
 
 const Projectstate = (props) => {
   const [studentFlag,setstudentFlag]=useState(false);
+  const [newUser,setNewUser]=useState(true);
   return (
     <projectcontext.Provider
-    value={{studentFlag,setstudentFlag}}>
+    value={{studentFlag,setstudentFlag,newUser,setNewUser}}>
         {props.children}
     </projectcontext.Provider>
 
